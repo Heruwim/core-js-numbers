efc9144aa9fb779dd902d621c3b2162746f3b242
Heruwim <heruwim25@gmail.com>
2023-11-20 17:40:35 +0200

    feat: implement function getFibonacciNumber

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -281,8 +281,21 @@ function getCube(num) {
  *   3  => 2
  *   10 => 55
  */
-function getFibonacciNumber(/* index */) {
-  throw new Error('Not implemented');
+function getFibonacciNumber(index) {
+  if (index <= 1) {
+    return index;
+  }
+
+  let previousNumber = 0;
+  let currentNumber = 1;
+
+  for (let i = 2; i <= index; i += 1) {
+    const nextNumber = previousNumber + currentNumber;
+    previousNumber = currentNumber;
+    currentNumber = nextNumber;
+  }
+
+  return currentNumber;
 }
 
 /**
