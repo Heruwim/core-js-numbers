2e1705e161838ba1f598d5250bab188c311f83ee
Heruwim <heruwim25@gmail.com>
2023-11-25 14:22:04 +0200

    feat: implement function roundToNearestInteger

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -558,8 +558,8 @@ function roundToSmallestInteger(number) {
  * 5.1  => 6
  * -5.9 => -5
  */
-function roundToLargestInteger(/* number */) {
-  throw new Error('Not implemented');
+function roundToLargestInteger(number) {
+  return Math.ceil(number);
 }
 
 /**
@@ -573,8 +573,8 @@ function roundToLargestInteger(/* number */) {
  * 5.4  => 5
  * -5.5 => -5
  */
-function roundToNearestInteger(/* number */) {
-  throw new Error('Not implemented');
+function roundToNearestInteger(number) {
+  return Math.round(number);
 }
 
 /**
