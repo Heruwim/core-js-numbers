0bcb9604ea64ad08d891afac8556d9a48857c04a
Heruwim <heruwim25@gmail.com>
2023-11-25 10:38:53 +0200

    feat: implement function isNumber

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -468,8 +468,8 @@ function getNumberValue(number) {
  * 5        => true
  * '5'      => false
  */
-function isNumber(/* number */) {
-  throw new Error('Not implemented');
+function isNumber(number) {
+  return number.isNumber();
 }
 
 /**
